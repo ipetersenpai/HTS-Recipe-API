74b8bdbb3a90970c5d12bc37f740a77b4865a1b5
Peter Francis Robante <robantepeterfran@gmail.com>
2023-08-16 23:18:07 +0800

    fix: structure style

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,26 +1,26 @@
-"""
-Django command to wait for the database to be available.
-"""
+"""Django command to wait for the database."""
 import time
 
 from psycopg2 import OperationalError as Psycopg20pError
-
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
-    """Django command to wait for database"""
+    """Django command to wait for the database."""
 
     def handle(self, *args, **options):
-        """Entrypoint for command."""
+        """Entrypoint for the command."""
         self.stdout.write('Waiting for database...')
         db_up = False
-        while db_up is False:
+        while not db_up:
             try:
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg20pError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-        
         self.stdout.write(self.style.SUCCESS('Database available!'))
+
+
+# Ensure there's a newline character at the end of the file
